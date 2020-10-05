5ff672e7413422baca6232162c8c2e0166b9e731
jdeleonardis <jdeleonardis@hotmail.com>
2020-10-05 13:43:07 -0400

    changed all to className and added tbody

--- client/src/pages/Resources/resources.js
+++ client/src/pages/Resources/resources.js
@@ -3,33 +3,35 @@ import React from "react";
 function Resources() {
  
     return (    
-        <main class="container">
-            <div class="row py-3">
-                <section class="col-lg-12">
+        <main className="container">
+            <div className="row py-3">
+                <section className="col-lg-12">
                     <header>
                         <h1>Resources</h1>
                     </header>        
                 </section>
             </div>    
 
-            <div class="row">
-                <section class="col-lg-12">
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Resource</th>
-                            <th>Website</th>
-                            <th>Phone Number</th>
-                        </tr>
-                        <tr>
-                            <td>Warriors At Ease</td>
-                            <td><a href="http://warriorsatease.org/" target="_blank" rel="noopener noreferrer">Warriors At Ease</a></td>
-                            <td>555-121-1212</td>
-                        </tr>
-                        <tr>
-                            <td>Suicide Prevention Lifeline</td>
-                            <td><a href="https://suicidepreventionlifeline.org/" target="_blank" rel="noopener noreferrer">Suicide Prevention Lifeline</a></td>              
-                            <td>555-121-1212</td>
-                        </tr>
+            <div className="row">
+                <section className="col-lg-12">
+                    <table className="table table-striped">
+                        <tbody>
+                            <tr>
+                                <th>Resource</th>
+                                <th>Website</th>
+                                <th>Phone Number</th>
+                            </tr>
+                            <tr>
+                                <td>Warriors At Ease</td>
+                                <td><a href="http://warriorsatease.org/" target="_blank" rel="noopener noreferrer">Warriors At Ease</a></td>
+                                <td>555-121-1212</td>
+                            </tr>
+                            <tr>
+                                <td>Suicide Prevention Lifeline</td>
+                                <td><a href="https://suicidepreventionlifeline.org/" target="_blank" rel="noopener noreferrer">Suicide Prevention Lifeline</a></td>              
+                                <td>555-121-1212</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </section>     
             </div>  
