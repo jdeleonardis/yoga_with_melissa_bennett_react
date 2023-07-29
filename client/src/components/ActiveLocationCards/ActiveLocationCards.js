c0363ad66a53725ba785bcec781576b62cfa6b9a
jdeleonardis <jdeleonardis@hotmail.com>
2023-07-29 17:49:51 -0500

    Added code to show when there are no locations enabled

--- client/src/components/ActiveLocationCards/ActiveLocationCards.js
+++ client/src/components/ActiveLocationCards/ActiveLocationCards.js
@@ -4,28 +4,100 @@ import CardDeck from 'react-bootstrap/CardDeck'
 import "./ActiveLocationCards.css"
 
 
-function ActiveLocationCards(props) {      
-  return (
-    <CardDeck className="cardDeckList">
-        {props.data.map((location,i) => (          
-            <div className="individualCardDivList" key={i}>  
+// function ActiveLocationCards(props) {      
+//   return (
+//     <CardDeck className="cardDeckList">
+//         {props.data.map((location,i) => (          
+//             <div className="individualCardDivList" key={i}>  
 
-                <Card>
-                    <Card.Body>
-                        <b>
-                        <Card.Text>
-                            {location.name}<br></br>
-                            {location.addr1}<br></br>
-                            {location.city}, {location.state} {location.zip}
-                            <br></br>
-                        </Card.Text>
-                        </b>
-                    </Card.Body>
-                </Card>
-            </div>
-        ))}
-    </CardDeck>
-)
-}
+//                 <Card>
+//                     <Card.Body>
+//                         <b>
+//                         <Card.Text>
+//                             {location.name}<br></br>
+//                             {location.addr1}<br></br>
+//                             {location.city}, {location.state} {location.zip}
+//                             <br></br>
+//                         </Card.Text>
+//                         </b>
+//                     </Card.Body>
+//                 </Card>
+//             </div>
+//         ))}
+//     </CardDeck>
+//     )
+// }
 
-export default ActiveLocationCards;
+// export default ActiveLocationCards;
+
+function ActiveLocationCards(props) {   
+    return (
+        <div>
+            {props.data.length > 0 ?                
+                <CardDeck className="cardDeckList">
+                    {props.data.map((location,i) => (          
+                        <div className="individualCardDivList" key={i}>  
+            
+                            <Card>
+                                <Card.Body>
+                                    <b>
+                                    <Card.Text>
+                                        {location.name}<br></br>
+                                        {location.addr1}<br></br>
+                                        {location.city}, {location.state} {location.zip}
+                                        <br></br>
+                                    </Card.Text>
+                                    </b>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))}
+                </CardDeck>                    
+            :
+                <div>
+                    <p>
+                        <h5>
+                            <ul>
+                                <center><li style={{listStyleType:"none"}}>No classes currently scheduled</li></center>
+                            </ul>
+                        </h5>
+                    </p>
+                </div>
+            }
+    </div>
+    )
+  }
+  
+  export default ActiveLocationCards;
+
+
+
+// {props.classData.length > 0
+//     ?      
+//     props.classData.map((classStuff,i) => (          
+//        <div          
+//          key={i}>
+//          <Row>             
+//            <Checkbox 
+//              classInfo={classStuff}
+//              onCheckboxChange={props.onCheckboxChange}
+//            />    
+//            <Address 
+//              classInfo={classStuff}
+//              mapClicked={props.mapClicked}
+//            />                            
+//          </Row>
+//        </div>
+//      ))
+//      : 
+//      <div>
+//        <p>
+//          <h5>
+//            <ul>
+//            <li style={{listStyleType:"none"}}>No classes currently scheduled</li>
+//            </ul>
+//          </h5>
+//        </p>
+//      </div>
+//     }
+
