3f4eb3ccd3b66fb4d64f7a45c8f986822a12ab02
jdeleonardis <jdeleonardis@hotmail.com>
2023-07-29 17:18:24 -0500

    added new date for my copyright

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
 return (
     <div className="footer">
-        <p id="footerfont">&copy; Jay DeLeonardis 2020</p>
+        <p id="footerfont">&copy; Jay DeLeonardis 2020-2023</p>
     </div>)
 }; 
 export default Footer;
